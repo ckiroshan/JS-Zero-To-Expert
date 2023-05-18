7709e92956bc72a109514153f361735d962bbf66
ckiroshan <ckiroshan@gmail.com>
2023-05-19 01:45:48 +0530

    JS Values & Variables.

---
+++ 1. JS-Fundamentals-01/starter/1. Values&Variables.js
@@ -0,0 +1,30 @@
+let js = "amazing";
+// if (js === "amazing") alert("JS is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+// <============ Values & Variables ============>
+
+let firstName = "Kane";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/**
+ * Some rules for naming variables =>
+ * Should be dome using camelCase.
+ * can't start with number,
+ * it can only contain numbers 123, letters, underscores _ or $ sign.
+ * can't use reserved keyword. (new, function, this)
+ * legal. (But not recommended to use),
+ *   => 'name' is also a reserved keyword but can be used.
+ *   => 'Brain' not start with a uppercase letter, but can be used. (Special purpose in OOP)
+ * Make them as descriptive as possible
+ * Variables that are all in uppercase are constants - (PI value won't change)
+ */
+
+let PI = 3.14523;
+console.log(typeof PI)
+
+
